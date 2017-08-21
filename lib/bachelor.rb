aa3d740088010a8b7d347c22dd23298c6337b134
Don Leistman <donleistman@gmail.com>
2017-08-21 16:03:25 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant_hash|
+      if contestant_hash["status"] == "Winner"
+          winner = contestant_hash["name"]
+          return winner.split[0]
+      end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, season_contestants|
+      season_contestants.each do |contestant_hash|
+          if contestant_hash["occupation"] == occupation
+              return contestant_hash["name"]
+          end
+      end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+    hometown_counter = 0
+    data.each do |season, season_contestants|
+        season_contestants.each do |contestant_hash|
+            if contestant_hash["hometown"] == hometown
+                hometown_counter += 1
+            end
+        end
+    end
+    return hometown_counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |season, season_contestants|
+        season_contestants.each do |contestant_hash|
+            if contestant_hash["hometown"] == hometown
+                return contestant_hash["occupation"]
+            end
+        end
+    end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+    age_counter = 0.0
+    age_sum = 0
+    data[season].each do |contestant_hash|
+        age_sum += contestant_hash["age"].to_f
+        age_counter += 1
+    end
+    average_age = (age_sum / age_counter)
+    if (average_age - average_age.to_i) >= 0.5
+        average_age = average_age.to_i + 1
+    else
+        average_age = average_age.to_i
+    end
+    return average_age
 end
